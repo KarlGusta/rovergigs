8492adcfdb77c4b8855ae5830cc78baafdc8c22e
Karlgusta Esimit <karlgustaesimit@gmail.com>
2024-10-27 13:48:59 +0300

    Adding more contents

--- post-a-job.php
+++ post-a-job.php
@@ -731,8 +731,11 @@
                 <div class="col-12 col-lg-auto mt-3 mt-lg-0">
                     <ul class="list-inline list-inline-dots mb-0">
                         <li class="list-inline-item">
-                            Copyright &copy; 2023
-                            <a href="www.weareremoteokay.com" class="link-secondary">We Are Remote Okay</a>.
+                            Copyright &copy;
+                            <script type="text/javascript">
+                                document.write(new Date().getFullYear());
+                            </script>
+                            <a href="www.rovergigs.com" class="link-secondary">Rover Gigs</a>.
                             All rights reserved.
                         </li>
                     </ul>
