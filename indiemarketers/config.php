e049e941949b1e79532f5f7ee00b060acd0fab4f
Karlgusta Esimit <karlgustaesimit@gmail.com>
2024-11-01 09:40:57 +0300

    Adding more contents

--- indiemarketers/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Config.php - Database configuration
- define('DB_SERVER', 'localhost');
- define('DB_USERNAME', 'root');
- define('DB_PASSWORD', '');
- define('DB_NAME', 'railsdevs');
- ?>
